2af4994b9a3827bcd0a6dd2b23733464e2ad4001
Evgeny Rodin <eugene.rodin1@gmail.com>
2018-11-25 22:37:30 +0300

    fix(missed-id): bug with empty tab id

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Don't waste your tabs
 
+[![Chrome Web Store Rating](https://img.shields.io/chrome-web-store/rating/hikfenppbcopnhaijlnmpfinpdihbcdm.svg)](https://chrome.google.com/webstore/detail/dont-waste-your-tabs/hikfenppbcopnhaijlnmpfinpdihbcdm)
+[![Chrome Web Store Stars](https://img.shields.io/chrome-web-store/stars/hikfenppbcopnhaijlnmpfinpdihbcdm.svg)](https://chrome.google.com/webstore/detail/dont-waste-your-tabs/hikfenppbcopnhaijlnmpfinpdihbcdm)
+[![Chrome Web Store Rating Count](https://img.shields.io/chrome-web-store/rating-count/hikfenppbcopnhaijlnmpfinpdihbcdm.svg)](https://chrome.google.com/webstore/detail/dont-waste-your-tabs/hikfenppbcopnhaijlnmpfinpdihbcdm)
+[![Chrome Web Store Downloads](https://img.shields.io/chrome-web-store/d/hikfenppbcopnhaijlnmpfinpdihbcdm.svg)](https://chrome.google.com/webstore/detail/dont-waste-your-tabs/hikfenppbcopnhaijlnmpfinpdihbcdm)
+[![Chrome Web Store Version](https://img.shields.io/chrome-web-store/v/hikfenppbcopnhaijlnmpfinpdihbcdm.svg)](https://chrome.google.com/webstore/detail/dont-waste-your-tabs/hikfenppbcopnhaijlnmpfinpdihbcdm)
+
 _Chrome extension that prevents opening multiple tabs for one hostname._
 
 ## Why
--- extension/background.js
+++ extension/background.js
@@ -1,26 +1,32 @@
 chrome.webNavigation.onCommitted.addListener(preventAddingDuplicateHostPage);
 
 function preventAddingDuplicateHostPage(details) {
-  // get common info about the tab because it might be created by another extension
-  chrome.tabs.get(details.tabId, function(currentTab) {
-    currentTab &&
-      chrome.tabs.query({}, function(tabs) {
-        var hostName = extractHostName(currentTab.url);
+  chrome.tabs.query({}, function(tabs) {
+    var hostName = isChromeExtensionNewTab(details.url)
+      ? 'chrome://newtab/'
+      : extractHostName(details.url);
 
-        for (let index = 0; index < tabs.length; index++) {
-          var existedTab = tabs[index];
-          var hasSameHostName = !existedTab.active && ~existedTab.url.indexOf(hostName);
+    for (let index = 0; index < tabs.length; index++) {
+      var existedTab = tabs[index];
+      var hasSameHostName = !existedTab.active && includes(existedTab.url, hostName);
 
-          if (hasSameHostName) {
-            chrome.tabs.update(existedTab.id, {active: true});
-            chrome.tabs.remove(currentTab.id);
-            return;
-          }
-        }
-      });
+      if (hasSameHostName) {
+        chrome.tabs.update(existedTab.id, {active: true});
+        chrome.tabs.remove(details.tabId);
+        return;
+      }
+    }
   });
 }
 
+function isChromeExtensionNewTab(url) {
+  return includes(url, 'newtab') && !/^(f|ht)tps?:\/\//i.test(url);
+}
+
 function extractHostName(url) {
-  return (~url.indexOf('//') ? url.split('/')[2] : url.split('/')[0]).split(':')[0].split('?')[0];
+  return (includes(url, '//') ? url.split('/')[2] : url.split('/')[0]).split(':')[0].split('?')[0];
+}
+
+function includes(str, substr) {
+  return ~str.indexOf(substr);
 }
